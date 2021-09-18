89cd4947d395945dd08f62186c3f85f50bd2c549
Nick <nicktehrany1@gmail.com>
2021-09-18 14:53:02 +0200

    Added basic usable polybar and bspwn scripts

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 killall -q polybar
-# Launch bar
-echo "---" | tee -a /tmp/polybar.log /tmp/polybar.log
-polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
+
+# Logs for debugging
+echo "---" | tee -a /tmp/polybar.log /tmp/polybar2.log
+
+if [[ "$(xrandr -q | grep -w "connected" | wc -l)" -gt "1" ]]; then 
+    polybar bar_dual 2>&1 | tee -a /tmp/polybar.log & disown
+    polybar bar_dual_external 2>&1 | tee -a /tmp/polybar2.log & disown
+else
+    polybar bar_single 2>&1 | tee -a /tmp/polybar.log & disown
+fi
 
 echo "Bars launched..."

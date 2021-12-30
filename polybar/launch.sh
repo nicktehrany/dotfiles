581eb6684b5e5e68762697404a116bd877b1920f
Nick <nicktehrany1@gmail.com>
2021-12-30 17:15:01 +0100

    Fixed polybar monitor detection

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,9 @@ echo "---" | tee -a /tmp/polybar.log /tmp/polybar2.log
 
 if [[ "$(xrandr -q | grep -w "connected" | wc -l)" -gt "1" ]]; then 
     SCREENS=($(xrandr --verbose | grep -Fw 'connected' | cut -d ' ' -f 1))
-    MONITOR=${SCREENS[1]}
+    MAIN_MONITOR=${SCREENS[0]}
+    EXT_MONITOR=${SCREENS[1]}
+    export MAIN_MONITOR EXT_MONITOR
     polybar bar_dual 2>&1 | tee -a /tmp/polybar.log & disown
     polybar bar_dual_external 2>&1 | tee -a /tmp/polybar2.log & disown
 else
